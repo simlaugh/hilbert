2b260e8dfb7b56003a7b0440d2b2269e0b60bdfc
simlaugh <49336400+simlaugh@users.noreply.github.com>
2021-08-06 12:03:42 +0800

    Update hilbert.py

--- hilbert.py
+++ hilbert.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import hilbert2, chirp
+from scipy.signal import hilbert2
 import csv
+
 rf_matrix = np.loadtxt(open("RF.csv","rb"),delimiter=',',skiprows=0)
 analytic_signal = hilbert2(rf_matrix,None)
-analytic_signal
+
+print(analytic_signal)
 
 hilbert_data = np.abs(analytic_signal)
 np.savetxt('Hilbert.csv',hilbert_data,delimiter=',')
-hilbert_data
+print(hilbert_data)
+plt.imshow(hilbert_data,cmap='hot',aspect='auto')  
+plt.show()
